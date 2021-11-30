3c08a50649e77c9264c1db2e6888782d130bd743
rubinni <novanitsuh@gmail.com>
2021-11-30 09:17:29 -0800

    Added Connection to DB

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const express = require('express')
-const port = 5000
-const app = express()
+const express = require('express');
+// const connection = require('connect.js')
+const port = 3306;
+const app = express();
+const mysql = require('mysql');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'ecom'
 
-app.get('/', (req,res) => {
-    
-})
+});
+
+connection.connect((err)=> {
+    if (err) {
+        return console.error('error' + err.message)
+    } 
+
+    console.log('Connected to the MYSQL Server. ');
+});
+
 
 app.listen(port, () => {
     console.log(`Server Running on port ${port}`);
-})
+});
